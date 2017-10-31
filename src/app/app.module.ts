b3a3d1c92931ac671fdcc3ade94a7edcf0abc877
sparshturkane <rajaturkane99@gmail.com>
2017-10-31 16:34:18 +0530

    added materalize theme in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { ParallaxComponent } from './components/parallax/parallax.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+
+const appRoutes: Routes = [
+  { path: 'home', component: ParallaxComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ParallaxComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
